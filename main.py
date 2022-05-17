f46d704afe4ac8b4a5e4a287b90e2038d5cfdd8d
Anoje Innocent <105368600+19932665@users.noreply.github.com>
2022-05-17 14:39:59 +0000

    leap year checker

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
-number=(int(input("number you want to check ")))
-if number % 2 == 0:
-  print("this is an even number")
+leap_year=(int(input("which year do you want to check ")))
+if leap_year % 4 == 0 :
+  if leap_year % 100 == 0:
+    if leap_year % 400 == 0:
+       print("leap year")
+    else:
+     print("not a leap year")
+  else:
+   print("leap year")
 else:
-  print("this is an odd number")
+  print("not a leap year")
